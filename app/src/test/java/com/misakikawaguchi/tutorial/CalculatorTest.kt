15c48f85ec60b7de29aba90c313a6a94650da1e5
川口美咲 <misaki@arcphilia.co.jp>
2021-03-01 22:12:10 +0900

    [Add]multiplyで掛け算の結果が取得できるテストを作成

--- app/src/test/java/com/misakikawaguchi/tutorial/CalculatorTest.kt
+++ app/src/test/java/com/misakikawaguchi/tutorial/CalculatorTest.kt
@@ -1,13 +1,20 @@
 package com.misakikawaguchi.tutorial
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import org.assertj.core.api.Assertions.*;
-import org.junit.Assert.*
 
 class CalculatorTest {
 
     @Test
-    fun multiply() {
+    fun multiplyで掛け算の掛け算の結果が取得できる() {
+        // テスト対象オブジェクト
+        val sut = Calculator()
+        // 実測値
+        val actual = sut.multiply(2, 3)
+        // 期待値
+        val expected = 6
+        // アサーション（実測値が期待値と等しい）
+        assertThat(actual).isEqualTo(expected)
     }
 
     @Test
