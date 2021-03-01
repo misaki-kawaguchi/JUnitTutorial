5a859f95426c1e2ad3a6f2664df2903c7cf66018
川口美咲 <misaki@arcphilia.co.jp>
2021-03-01 22:16:23 +0900

    [Add]メソッド名を具体的にする、テストをもう1つ追加して検証

--- app/src/test/java/com/misakikawaguchi/tutorial/CalculatorTest.kt
+++ app/src/test/java/com/misakikawaguchi/tutorial/CalculatorTest.kt
@@ -6,13 +6,25 @@ import org.junit.Test
 class CalculatorTest {
 
     @Test
-    fun multiplyで掛け算の結果が取得できる() {
+    fun multiplyで2と3を掛けたら6が取得できる() {
         // テスト対象オブジェクト
         val sut = Calculator()
         // 実測値
         val actual = sut.multiply(2, 3)
         // 期待値
         val expected = 6
+        // アサーション（実測値が期待値と等しい）
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun multiplyで0と3を掛けたら0が取得できる() {
+        // テスト対象オブジェクト
+        val sut = Calculator()
+        // 実測値
+        val actual = sut.multiply(0, 3)
+        // 期待値
+        val expected = 0
         // アサーション（実測値が期待値と等しい）
         assertThat(actual).isEqualTo(expected)
     }
