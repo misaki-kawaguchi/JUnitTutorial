8811c011c977b0c569362b7a1a8642ee02736080
川口美咲 <misaki@arcphilia.co.jp>
2021-03-01 21:09:40 +0900

    [Add]CalculatorTestを作成

--- app/src/main/java/com/misakikawaguchi/tutorial/Tutorial.kt
+++ app/src/main/java/com/misakikawaguchi/tutorial/Tutorial.kt
@@ -1,2 +1,14 @@
 package com.misakikawaguchi.tutorial
 
+// テスト対処クラス
+class Calculator {
+    // 掛け算
+    fun multiply(x: Int, y: Int): Int {
+        return x * y
+    }
+
+    // 割り算
+    fun divide(x: Int, y: Int): Int {
+        return x / y
+    }
+}
---
+++ app/src/test/java/com/misakikawaguchi/tutorial/CalculatorTest.kt
@@ -0,0 +1,16 @@
+package com.misakikawaguchi.tutorial
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CalculatorTest {
+
+    @Test
+    fun multiply() {
+    }
+
+    @Test
+    fun divide() {
+    }
+}
